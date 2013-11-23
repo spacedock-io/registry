ff5f6c43d8ae70e5775c7f4e2c9a6818cbde8b3a
yawnt <yawn.localhost@gmail.com>
2013-11-23 19:43:35 +0100

    fix: env

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    pkg     = require('./package.json'),
     routes  = require('./routes/');
 
 var server = express();
@@ -7,6 +8,12 @@ server.use(express.urlencoded());
 server.use(express.json());
 server.use(express.cookieParser());
 server.use(express.session({ secret: require('node-uuid').v4() }));
+
+server.use(function(req, res, next) {
+  res.header('X-Docker-Registry-Version', pkg.version);
+  res.header('X-Docker-Registry-Config', (process.env['SPACEDOCK_REGISTRY_ENV'] || 'staging'));
+  next();
+});
 
 routes.hookRoutes(server);
 
