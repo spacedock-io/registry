4a728979daf1acc673c125849843d6c9c9796ade
yawnt <yawn.localhost@gmail.com>
2013-12-26 19:11:30 +0100

    fix: refactor auth

--- auth/auth.go
+++ auth/auth.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-  "fmt"
   "net/http"
   "encoding/gob"
   "regexp"
@@ -14,32 +13,58 @@ var parseToken = regexp.MustCompile(`^Token signature=(\w+),repository=(.*?),acc
 
 type callback func(http.ResponseWriter, *http.Request)
 
+func init() {
+  gob.Register(&Token{})
+}
+
 type Token struct {
   Signature string
   Repo      string
   Access    string
 }
 
-func init() {
-  gob.Register(&Token{})
+func LoadCheckToken(s *sessions.Session, w http.ResponseWriter, r *http.Request) bool {
+  if r.Header["Authorization"] == nil {
+    return false
+  } else {
+    extracted := parseToken.FindStringSubmatch(r.Header["Authorization"][0])
+    s.Values["token"] = &Token{
+      Signature: extracted[1],
+      Repo: extracted[2],
+      Access: extracted[3],
+    }
+    s.Save(r, w)
+    return s.Values["token"].(*Token).Validate()
+  }
+}
+
+func (t *Token) Header() string {
+  return "Token signature=" + t.Signature + ",repository=" + t.Repo + ",access="+ t.Access
+}
+
+func (t *Token) Validate() bool {
+  client := &http.Client{}
+  req, _ := http.NewRequest("GET", "https://index.docker.io/v1/repositories/" + t.Repo + "/images", nil)
+  req.Header.Add("Authorization", t.Header())
+  resp, err := client.Do(req)
+  if err != nil || resp.StatusCode != 200 {
+    return false
+  } else {
+    return true
+  }
 }
 
 func Secure(c *mux.Router) callback {
   return func(w http.ResponseWriter, r *http.Request) {
+    /*
+     * These two routes require no auth
+     */
     if r.URL.String() != "/" && r.URL.String() != "/v1/_ping" {
       session, _ := Store.Get(r, "default")
-      if session.Values["token"] != nil || r.Header["Authorization"] != nil {
-        if(session.Values["token"] == nil) {
-          extracted := parseToken.FindStringSubmatch(r.Header["Authorization"][0])
-          fmt.Println(extracted)
-          session.Values["token"] = &Token{
-            Signature: extracted[1],
-            Repo: extracted[2],
-            Access: extracted[3],
-          }
-          session.Save(r, w)
-          fmt.Println(session.Values["token"].(*Token).Access)
-        }
+      /*
+       * Two types of auth are valid: Token or Session 
+       */
+      if session.Values["token"] != nil || LoadCheckToken(session, w, r) {
         c.ServeHTTP(w, r);
       } else {
         w.WriteHeader(401);
